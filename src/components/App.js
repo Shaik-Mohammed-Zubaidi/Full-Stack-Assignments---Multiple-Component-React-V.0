57fde367e6d3d31e8ca273b39b7026e44134a779
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-10-18 23:29:42 +0530

    mccommit

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,20 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 class App extends Component {
-    render() {
-
-        return(
-            <>
-               
-            </>
-        )
-    }
+  render() {
+    return (
+      <>
+        <h1 data- ns- test="project- name">
+          What To Do!
+        </h1>
+        <p data- ns- test="project- description">
+          A site which will show you a list of things you can do from games to
+          courses and also keeps tracks of your progresses
+        </p>
+      </>
+    );
+  }
 }
-
 
 export default App;
