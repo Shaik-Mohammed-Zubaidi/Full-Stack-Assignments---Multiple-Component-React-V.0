5c73506e3f841eba329ca6223b3016410c4cb3b7
Shaik-Mohammed-Zubaidi <68868510+Shaik-Mohammed-Zubaidi@users.noreply.github.com>
2020-10-18 23:33:55 +0530

    mcrcommit

--- src/components/App.js
+++ src/components/App.js
@@ -4,15 +4,15 @@ import "../styles/App.css";
 class App extends Component {
   render() {
     return (
-      <>
-        <h1 data- ns- test="project- name">
+      <div data-ns-test>
+        <h1 data-ns-test="project-name">
           What To Do!
         </h1>
-        <p data- ns- test="project- description">
+        <p data-ns-test="project-description">
           A site which will show you a list of things you can do from games to
           courses and also keeps tracks of your progresses
         </p>
-      </>
+      </div>
     );
   }
 }
